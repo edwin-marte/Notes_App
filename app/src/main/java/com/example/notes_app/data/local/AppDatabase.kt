be812434636372ec32f50bcb5cccaa43cdbc0d0e
Edwin <edwin25mt@gmail.com>
2023-05-13 11:11:41 -0400

    create app database

---
+++ app/src/main/java/com/example/notes_app/data/local/AppDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.notes_app.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.notes_app.data.model.NoteEntity
+
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun noteDao(): NotesDao
+}
