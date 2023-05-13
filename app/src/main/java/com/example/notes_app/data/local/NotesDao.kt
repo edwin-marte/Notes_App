b1b8a2ac60ca5580d9c3e7000234c3e1ee0ace66
Edwin <edwin25mt@gmail.com>
2023-05-13 11:24:09 -0400

    create room data access object

---
+++ app/src/main/java/com/example/notes_app/data/local/NotesDao.kt
@@ -0,0 +1,16 @@
+package com.example.notes_app.data.local
+
+import androidx.room.*
+import com.example.notes_app.data.model.NoteEntity
+
+@Dao
+interface NotesDao {
+    @Query("SELECT * FROM noteEntity")
+    suspend fun getAllNotes():List<NoteEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note:NoteEntity)
+
+    @Delete
+    suspend fun delete(note: NoteEntity)
+}
