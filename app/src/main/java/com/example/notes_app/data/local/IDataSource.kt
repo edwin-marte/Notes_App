ae51e232df2d2c67a782cec9d48868ec870827de
Edwin <edwin25mt@gmail.com>
2023-05-13 11:17:16 -0400

    create datasource implementation

---
+++ app/src/main/java/com/example/notes_app/data/local/IDataSource.kt
@@ -0,0 +1,19 @@
+package com.example.notes_app.data.local
+
+import com.example.notes_app.data.model.NoteEntity
+import com.example.notes_app.core.Resource
+import javax.inject.Inject
+
+class IDataSource @Inject constructor(private val appDatabase: AppDatabase): DataSource {
+    override suspend fun getNotes(): Resource<List<NoteEntity>> {
+        return Resource.Success(appDatabase.noteDao().getAllNotes())
+    }
+
+    override suspend fun insertNoteIntoRoom(note:NoteEntity) {
+        appDatabase.noteDao().insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: NoteEntity) {
+        appDatabase.noteDao().delete(note)
+    }
+}
