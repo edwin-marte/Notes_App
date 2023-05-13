9eeed7e909164564f3d266ac62816fe060ccb749
Edwin <edwin25mt@gmail.com>
2023-05-13 11:34:46 -0400

    create resource sealed class

---
+++ app/src/main/java/com/example/notes_app/core/Resource.kt
@@ -0,0 +1,7 @@
+package com.example.notes_app.core
+
+sealed class Resource<T> {
+    class Loading<T> : Resource<T>()
+    data class Success<T>(val data: T) : Resource<T>()
+    data class Failure<T>(val exception: Exception) : Resource<T>()
+}
