7f0d6e142e8f71684f34d1ce5914dca1f4d1c9de
Edwin <edwin25mt@gmail.com>
2023-05-13 11:22:03 -0400

    create main view model

---
+++ app/src/main/java/com/example/notes_app/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.notes_app.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.example.notes_app.data.model.NoteEntity
+import com.example.notes_app.domain.Repository
+import com.example.notes_app.core.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor (private val repository: Repository): ViewModel() {
+    fun saveNote(note:NoteEntity) {
+        viewModelScope.launch {
+            repository.insertNote(note)
+        }
+    }
+
+    fun deleteNote(note: NoteEntity) {
+        viewModelScope.launch {
+            repository.deleteNote(note)
+        }
+    }
+
+    fun getNotes() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try {
+            emit(repository.getNotes())
+        } catch (e: java.lang.Exception) {
+            emit(Resource.Failure(e))
+        }
+    }
+}
